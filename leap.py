d34a20fba19eafa06eb42fb81ea27b14320be79d
Tharaah <84385275+Tharaah@users.noreply.github.com>
2021-05-21 13:22:06 +0200

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,14 @@
+year = int(input ("Enter a year: \n"))
+
+
+if year % 400 == 0:
+  print (  str(year), "is a leap year.")
+
+elif year % 4 == 0:
+  if year % 100 != 0:
+    print( str(year), "is a leap year.")
+  else: 
+    print( str(year) , "is not a leap year.")
+
+else : 
+  print ( str(year) , "is not a leap year.")
